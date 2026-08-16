9fb9d2396b43e06cd98a83b88869c1b8650f66de
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-17 05:20:48 +0800

    Update SignalBotApplication.kt

--- app/src/main/java/com/farhad/signalbot/SignalBotApplication.kt
+++ app/src/main/java/com/farhad/signalbot/SignalBotApplication.kt
@@ -2,15 +2,33 @@ package com.farhad.signalbot
 
 import android.app.Application
 import com.farhad.signalbot.core.di.AppContainer
+import com.farhad.signalbot.core.di.DatabaseProvider
+import com.farhad.signalbot.data.RoomSignalHistoryStore
+import com.farhad.signalbot.data.SignalHistoryRepository
 
 class SignalBotApplication : Application() {
 
     lateinit var appContainer: AppContainer
         private set
 
+    lateinit var historyRepository: SignalHistoryRepository
+        private set
+
     override fun onCreate() {
         super.onCreate()
 
         appContainer = AppContainer(this)
+
+        val database = DatabaseProvider.get(this)
+
+        val historyStore =
+            RoomSignalHistoryStore(
+                dao = database.signalDao()
+            )
+
+        historyRepository =
+            SignalHistoryRepository(
+                store = historyStore
+            )
     }
 }
