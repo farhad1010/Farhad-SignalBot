c398f6df09fb81d3d9940ff65db1ebaa90f5a749
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-17 01:19:58 +0800

    Update SignalHistoryStore.kt

--- app/src/main/java/com/farhad/signalbot/data/SignalHistoryStore.kt
+++ app/src/main/java/com/farhad/signalbot/data/SignalHistoryStore.kt
@@ -1,11 +1,11 @@
 package com.farhad.signalbot.data
 
 import com.farhad.signalbot.core.model.SignalRecord
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 
 interface SignalHistoryStore {
 
-    val signals: StateFlow<List<SignalRecord>>
+    val signals: Flow<List<SignalRecord>>
 
     suspend fun add(record: SignalRecord)
 
