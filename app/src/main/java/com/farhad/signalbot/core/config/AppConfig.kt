d8ebf971e1c6b7870d87aa6c26681d286bf74b76
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-17 00:45:40 +0800

    Create AppConfig.kt

---
+++ app/src/main/java/com/farhad/signalbot/core/config/AppConfig.kt
@@ -0,0 +1,14 @@
+package com.farhad.signalbot.core.config
+
+object AppConfig {
+
+    const val MARKET_API_BASE_URL = "https://api.polygon.io/"
+
+    const val DEFAULT_CANDLE_LIMIT = 200
+
+    const val DEFAULT_TIMEFRAME_SECONDS = 60L
+
+    const val NETWORK_TIMEOUT_SECONDS = 20L
+
+    const val MINIMUM_CANDLES_REQUIRED = 50
+}
