00b1af1edee26ffc3e12f7056117d35a8157db74
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-18 05:08:56 +0800

    Update SupportedSymbols.kt

--- app/src/main/java/com/farhad/signalbot/core/model/SupportedSymbols.kt
+++ app/src/main/java/com/farhad/signalbot/core/model/SupportedSymbols.kt
@@ -3,41 +3,201 @@ package com.farhad.signalbot.core.model
 object SupportedSymbols {
 
     val all: List<TradingSymbol> = listOf(
+
         TradingSymbol(
-            id = "EURUSD",
-            displayName = "EUR/USD",
-            providerSymbol = "C:EURUSD",
-            quoteCurrency = "USD"
+            "EURUSD",
+            "EUR/USD",
+            "C:EURUSD",
+            "USD"
         ),
+
         TradingSymbol(
-            id = "GBPUSD",
-            displayName = "GBP/USD",
-            providerSymbol = "C:GBPUSD",
-            quoteCurrency = "USD"
+            "GBPUSD",
+            "GBP/USD",
+            "C:GBPUSD",
+            "USD"
         ),
+
         TradingSymbol(
-            id = "USDJPY",
-            displayName = "USD/JPY",
-            providerSymbol = "C:USDJPY",
-            quoteCurrency = "JPY"
+            "USDJPY",
+            "USD/JPY",
+            "C:USDJPY",
+            "JPY"
         ),
+
         TradingSymbol(
-            id = "AUDUSD",
-            displayName = "AUD/USD",
-            providerSymbol = "C:AUDUSD",
-            quoteCurrency = "USD"
+            "AUDUSD",
+            "AUD/USD",
+            "C:AUDUSD",
+            "USD"
         ),
+
         TradingSymbol(
-            id = "USDCAD",
-            displayName = "USD/CAD",
-            providerSymbol = "C:USDCAD",
-            quoteCurrency = "CAD"
+            "USDCAD",
+            "USD/CAD",
+            "C:USDCAD",
+            "CAD"
         ),
+
         TradingSymbol(
-            id = "USDCHF",
-            displayName = "USD/CHF",
-            providerSymbol = "C:USDCHF",
-            quoteCurrency = "CHF"
+            "USDCHF",
+            "USD/CHF",
+            "C:USDCHF",
+            "CHF"
+        ),
+
+        TradingSymbol(
+            "NZDUSD",
+            "NZD/USD",
+            "C:NZDUSD",
+            "USD"
+        ),
+
+        TradingSymbol(
+            "EURGBP",
+            "EUR/GBP",
+            "C:EURGBP",
+            "GBP"
+        ),
+
+        TradingSymbol(
+            "EURJPY",
+            "EUR/JPY",
+            "C:EURJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "EURCHF",
+            "EUR/CHF",
+            "C:EURCHF",
+            "CHF"
+        ),
+
+        TradingSymbol(
+            "EURAUD",
+            "EUR/AUD",
+            "C:EURAUD",
+            "AUD"
+        ),
+
+        TradingSymbol(
+            "EURCAD",
+            "EUR/CAD",
+            "C:EURCAD",
+            "CAD"
+        ),
+
+        TradingSymbol(
+            "EURNZD",
+            "EUR/NZD",
+            "C:EURNZD",
+            "NZD"
+        ),
+
+        TradingSymbol(
+            "GBPJPY",
+            "GBP/JPY",
+            "C:GBPJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "GBPCHF",
+            "GBP/CHF",
+            "C:GBPCHF",
+            "CHF"
+        ),
+
+        TradingSymbol(
+            "GBPAUD",
+            "GBP/AUD",
+            "C:GBPAUD",
+            "AUD"
+        ),
+
+        TradingSymbol(
+            "GBPCAD",
+            "GBP/CAD",
+            "C:GBPCAD",
+            "CAD"
+        ),
+
+        TradingSymbol(
+            "GBPNZD",
+            "GBP/NZD",
+            "C:GBPNZD",
+            "NZD"
+        ),
+
+        TradingSymbol(
+            "AUDJPY",
+            "AUD/JPY",
+            "C:AUDJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "AUDCAD",
+            "AUD/CAD",
+            "C:AUDCAD",
+            "CAD"
+        ),
+
+        TradingSymbol(
+            "AUDCHF",
+            "AUD/CHF",
+            "C:AUDCHF",
+            "CHF"
+        ),
+
+        TradingSymbol(
+            "AUDNZD",
+            "AUD/NZD",
+            "C:AUDNZD",
+            "NZD"
+        ),
+
+        TradingSymbol(
+            "CADJPY",
+            "CAD/JPY",
+            "C:CADJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "CADCHF",
+            "CAD/CHF",
+            "C:CADCHF",
+            "CHF"
+        ),
+
+        TradingSymbol(
+            "CHFJPY",
+            "CHF/JPY",
+            "C:CHFJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "NZDJPY",
+            "NZD/JPY",
+            "C:NZDJPY",
+            "JPY"
+        ),
+
+        TradingSymbol(
+            "NZDCAD",
+            "NZD/CAD",
+            "C:NZDCAD",
+            "CAD"
+        ),
+
+        TradingSymbol(
+            "NZDCHF",
+            "NZD/CHF",
+            "C:NZDCHF",
+            "CHF"
         )
     )
 }
