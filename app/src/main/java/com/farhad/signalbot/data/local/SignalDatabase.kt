ab38af0fe282efcb61abfa4ebb97f51b319e7d82
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-18 03:08:42 +0800

    Update SignalDatabase.kt

--- app/src/main/java/com/farhad/signalbot/data/local/SignalDatabase.kt
+++ app/src/main/java/com/farhad/signalbot/data/local/SignalDatabase.kt
@@ -4,9 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [SignalEntity::class],
+    entities = [
+        SignalEntity::class
+    ],
     version = 1,
-    exportSchema = true
+    exportSchema = false
 )
 abstract class SignalDatabase : RoomDatabase() {
 
