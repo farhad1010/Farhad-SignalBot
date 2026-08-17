9b1f1b1fc6bc0e5a2c9d6c8fe93b37589f0d5d87
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-18 05:05:51 +0800

    Update MarketApiService.kt

--- app/src/main/java/com/farhad/signalbot/data/remote/MarketApiService.kt
+++ app/src/main/java/com/farhad/signalbot/data/remote/MarketApiService.kt
@@ -7,12 +7,9 @@ import retrofit2.http.Query
 interface MarketApiService {
 
     @GET(
-        "v2/aggs/ticker/" +
-            "{ticker}/range/" +
-            "{multiplier}/{timespan}/" +
-            "{from}/{to}"
+        "v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from}/{to}"
     )
-    suspend fun getMinuteBars(
+    suspend fun getBars(
         @Path("ticker")
         ticker: String,
 
@@ -20,7 +17,7 @@ interface MarketApiService {
         multiplier: Int = 1,
 
         @Path("timespan")
-        timespan: String = "minute",
+        timespan: String = "second",
 
         @Path("from")
         from: String,
@@ -32,9 +29,20 @@ interface MarketApiService {
         adjusted: Boolean = false,
 
         @Query("sort")
-        sort: String = "asc",
+        sort: String = "desc",
 
         @Query("limit")
         limit: Int = 5000
+    ): PolygonBarsResponse
+
+    @GET(
+        "v2/aggs/ticker/{ticker}/prev"
+    )
+    suspend fun getPreviousDayBar(
+        @Path("ticker")
+        ticker: String,
+
+        @Query("adjusted")
+        adjusted: Boolean = false
     ): PolygonBarsResponse
 }
