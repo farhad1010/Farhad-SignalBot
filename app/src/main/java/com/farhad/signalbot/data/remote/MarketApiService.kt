19c2e2efea505f88b007ea6edc119f01ded8e126
farhad1010 <120691616+farhad1010@users.noreply.github.com>
2026-08-18 13:11:23 +0800

    Update MarketApiService.kt

--- app/src/main/java/com/farhad/signalbot/data/remote/MarketApiService.kt
+++ app/src/main/java/com/farhad/signalbot/data/remote/MarketApiService.kt
@@ -6,43 +6,21 @@ import retrofit2.http.Query
 
 interface MarketApiService {
 
-    @GET(
-        "v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from}/{to}"
-    )
+    @GET("v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from}/{to}")
     suspend fun getBars(
-        @Path("ticker")
-        ticker: String,
-
-        @Path("multiplier")
-        multiplier: Int = 1,
-
-        @Path("timespan")
-        timespan: String = "second",
-
-        @Path("from")
-        from: String,
-
-        @Path("to")
-        to: String,
-
-        @Query("adjusted")
-        adjusted: Boolean = false,
-
-        @Query("sort")
-        sort: String = "desc",
-
-        @Query("limit")
-        limit: Int = 5000
+        @Path("ticker") ticker: String,
+        @Path("multiplier") multiplier: Int,
+        @Path("timespan") timespan: String,
+        @Path("from") from: String,
+        @Path("to") to: String,
+        @Query("adjusted") adjusted: Boolean = false,
+        @Query("sort") sort: String = "desc",
+        @Query("limit") limit: Int = 5000
     ): PolygonBarsResponse
 
-    @GET(
-        "v2/aggs/ticker/{ticker}/prev"
-    )
+    @GET("v2/aggs/ticker/{ticker}/prev")
     suspend fun getPreviousDayBar(
-        @Path("ticker")
-        ticker: String,
-
-        @Query("adjusted")
-        adjusted: Boolean = false
+        @Path("ticker") ticker: String,
+        @Query("adjusted") adjusted: Boolean = false
     ): PolygonBarsResponse
 }
